e7633593a70fe16a0fbd11daa2f47c92a6b13283
Mathews Alufwani <mathewalufwani@gmail.com>
2020-05-14 11:40:01 +0300

    add functionality to form component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,9 +11,9 @@ export class QuotesComponent implements OnInit {
 
 
   quotes:Quote[] = [
-    new Quote('“Do I not destroy my enemies when I make them my friends?”', 0, 0, 'Abraham Lincoln', 'John McGraw', new Date(2020,1,14)),
-    new Quote('"Nothing in life is promised except death."', 0, 0, 'Kanye West', 'David Boseman', new Date(2020,2,14)),
-    new Quote('"No human is limited."', 0, 0, 'Eliud Kipchoge', 'Joseph Kamau', new Date(2020,3,14)),
+    new Quote('“Do I not destroy my enemies when I make them my friends?”', 18, 2, 'Abraham Lincoln', 'John McGraw', new Date(2020,1,14)),
+    new Quote('"Nothing in life is promised except death."', 7, 1, 'Kanye West', 'David Boseman', new Date(2020,2,14)),
+    new Quote('"No human is limited."', 15, 0, 'Eliud Kipchoge', 'Joseph Kamau', new Date(2020,3,14)),
   ];
 
   toggleDetails(index){
@@ -28,6 +28,11 @@ export class QuotesComponent implements OnInit {
         this.quotes.splice(index,1)
       }
     }
+  }
+
+  addNewQuote(quote){
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
 
   constructor() { }
